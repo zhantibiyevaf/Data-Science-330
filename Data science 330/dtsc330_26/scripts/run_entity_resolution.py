2d288d01d94a601aaa4997dcf5166ef53b5dc6f6
Fatima Zhantibiyeva <the.fatimazhan@gmail.com>
2026-03-23 19:19:35 -0400

    Week 8

---
+++ Data science 330/dtsc330_26/scripts/run_entity_resolution.py
@@ -0,0 +1,14 @@
+# 0. Train our classifier
+
+def train_eres_classifier():
+    # load the training data
+    df = pd.read_csv()
+
+
+#1. read in batches of data
+#we'll loop through authors (larger dataset) and compare against grantees
+
+
+
+# Everything is within a batch/block
+# 2. Compute distances within this block
